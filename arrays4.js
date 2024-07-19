86eecaacd05f891c30a66f09a4e83afd728bda62
Mateusvspm <sousamateusv@gmail.com>
2024-07-19 15:45:52 -0300

    atv-4 arrays

---
+++ arrays4.js
@@ -0,0 +1,6 @@
+// Crie um array com os elementos ['vermelho', 'verde']. Use o método unshift() para adicionar os elementos 'azul' e 'amarelo' no início do array. Exiba o array resultante.
+
+const elemento = ['vermelho','verde']
+elemento.unshift('azul','amarelo')
+
+console.log(elemento)
