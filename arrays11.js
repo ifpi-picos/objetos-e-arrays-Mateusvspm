63994b49af24831125695eff8e01953e5e03a353
Mateusvspm <sousamateusv@gmail.com>
2024-07-25 17:06:20 -0300

    atv-11 arrays

---
+++ arrays11.js
@@ -0,0 +1,7 @@
+//Utilize a lista de filmes do exercício anterior e exiba no console cada um dos filmes informados usando um for..of.
+
+let filmes = ['Forrest Gump','O Rei Leão','O Senhor dos Anéis','Batman','À Espera de um Milagre'];
+
+for(let filme of filmes) {
+    console.log(filme)
+}
