ddef5d1ddd438150fc1296c3bc355dc7bebaa62a
Mateusvspm <sousamateusv@gmail.com>
2024-07-26 15:13:49 -0300

    atv-17 arrays

---
+++ arrays17.js
@@ -0,0 +1,6 @@
+//Crie uma string com o seu nome e imprima o número de caracteres.
+
+let nome = 'Mateus'
+let numCarcteres = nome.length
+
+console.log(`o número de caracteres do nome ${nome} é: ${numCarcteres}`)
