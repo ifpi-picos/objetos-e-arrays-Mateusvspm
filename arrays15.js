ec89a3570b760f055ac8539e93a40a3ad810e152
Mateusvspm <sousamateusv@gmail.com>
2024-07-25 22:48:13 -0300

    atv-15 arrays

---
+++ arrays15.js
@@ -0,0 +1,5 @@
+//Crie um array de frutas e use includes para verificar se a fruta "maçã" está presente no array.
+
+const frutas = ['uva','pera','abacaxi','mamão']
+
+console.log(frutas.includes('maçã'))
