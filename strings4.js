ce83a8d0b09c4f225066a46e775d9c1820cdfe5a
Mateusvspm <sousamateusv@gmail.com>
2024-07-28 19:46:46 -0300

    atv-3 strings

---
+++ strings4.js
@@ -0,0 +1,5 @@
+//Crie uma string com uma frase e use o método `toUpperCase` para imprimir a frase em letras maiúsculas.
+
+let frase = 'O homem é o lobo do homem'
+
+console.log(frase.toUpperCase())
